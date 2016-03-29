8cc0b314c1c59e4491c44e91dec7f4bc3e153d0e
carlos calderon <carlosdevelopment1979@gmail.com>
2016-03-29 00:27:11 -0500

    refactor setImageToUIImageView in answersView

--- just-query/AnswersController.m
+++ just-query/AnswersController.m
@@ -22,14 +22,14 @@
     self.questionBody.contentInset = UIEdgeInsetsZero;
     self.automaticallyAdjustsScrollViewInsets = false;
     self.questionBody.text = self.questionBodyDescription;
+    
+    [self setImageToUIImageView:self.questionUserImage fromUserId:self.questionUserIdKey];
 
     
     Dataservice *dataService = [Dataservice sharedDataservice];
-    
     Firebase *questionRef = [dataService.questionsRef childByAppendingPath:self.questionKey];
     
     Firebase *answersForQuestionRef = [questionRef childByAppendingPath:@"answers"];
-    
     [answersForQuestionRef observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
         NSArray *snapShots = snapshot.children.allObjects;
                 for (FDataSnapshot *snap in snapShots) {
@@ -43,18 +43,15 @@
                 NSLog(@"Answer2 Key Destination %@", snap2.key);
                 
                 NSDictionary *answer = snap2.value;
-                
                 if([answer isKindOfClass:[NSDictionary class]]){
                     NSString *answerKey = [[answer allKeys] objectAtIndex:0];
                     NSLog(@"Answer2 Value Destination %@", answerKey);
                     Firebase *answerRef = [dataService.answersRef childByAppendingPath:answerKey];
                     
-                    
                     [answerRef observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot2) {
                         NSLog(@"Content Answer Destination:::::  %@", snapshot2.value);
                         
                         Answer *answer = [[Answer alloc] init:snapshot2.key dictionary:snapshot2.value];
-                        
                         
                         if (![self containAnswer:answer]) {
                             [self.answers addObject: answer];
@@ -95,6 +92,10 @@
     if (cell == nil) {
         
         cell = [[AnswerCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    } else {
+        //reset Image
+        UIImage *btnImage = [UIImage imageNamed:@"thumbsUpE.png"];
+        [cell.answerUpVoteButton setImage:btnImage forState:UIControlStateNormal];
     }
     
     cell.answerUpVoteButton.tag = indexPath.row;
@@ -122,25 +123,20 @@
                 NSString *profileKey = [[profile allKeys] objectAtIndex:0];
                 NSString *userId = dataService.currentUserRef.key;
                 NSLog(@" dic Key %@", profileKey);
+                
+                [self setImageToUIImageView:cell.answerUserImage fromUserId:profileKey];
                 NSLog(@" UserIdd %@", userId);
                 if ([profileKey isEqualToString:userId]) {
-                    UIImage *btnImage = [UIImage imageNamed:@"thumbUpF.png"];
+                    UIImage *btnImage = [UIImage imageNamed:@"thumbsUpF.png"];
                     [cell.answerUpVoteButton setImage:btnImage forState:UIControlStateNormal];
-                    
                 }
                 NSLog(@" dic Key %@", [[profile allKeys] objectAtIndex:0]);
-
             }
         }
 
     } withCancelBlock:^(NSError *error) {
         NSLog(@"%@", error.description);
     }];
-
-
-    //cell.textLabel.text = [[self.answers objectAtIndex:indexPath.row] answerDescription];
-    
-    //cell.imageView.image = [UIImage imageNamed:@"geekPic.jpg"];
     
     return cell;
 }
@@ -171,7 +167,7 @@
     Firebase *answerUpvotes = [answerRef childByAppendingPath:@"upvotes"];
     // Attach a block to read the data at our posts reference
     [answerUpvotes observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
-        NSLog(@" UPVAOTES %@", snapshot.value);
+        NSLog(@" UPVOTES %@", snapshot.value);
         NSArray *snapShots = snapshot.children.allObjects;
         BOOL upvoted = NO;
         for (FDataSnapshot *snap in snapShots) {
@@ -190,7 +186,6 @@
                     upvoted = YES;
                 }
                 NSLog(@" dic Key %@", [[profile allKeys] objectAtIndex:0]);
-                
             }
         }
         if (upvoted == NO) {
@@ -206,6 +201,59 @@
         NSLog(@"%@", error.description);
     }];
 
+}
+
+- (void)setImageToUIImageView:(UIImageView *)imageView fromUserId:(NSString *)userId
+{
+    
+    Dataservice *dataService = [Dataservice sharedDataservice];
+    Firebase *usersRef = [dataService.usersRef childByAppendingPath:userId];
+    Firebase *userProfileRef = [usersRef childByAppendingPath:@"profile"];
+    
+    
+    // Attach a block to read the data at our posts reference
+    [userProfileRef observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
+        
+        NSDictionary *profile = snapshot.value;
+        
+        if([profile isKindOfClass:[NSDictionary class]]){
+            
+            NSString *profileKey = [[profile allKeys] objectAtIndex:0];
+            NSLog(@"%@", [[profile allKeys] objectAtIndex:0]);
+            
+            Firebase *userProfileRef2 = [dataService.profilesRef childByAppendingPath:profileKey];
+            // Attach a block to read the data at our posts reference
+            [userProfileRef2 observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
+                NSLog(@"%@", snapshot.value);
+                
+                if([snapshot.value isKindOfClass:[NSDictionary class]]){
+                    
+                    NSString *imageUrl = [snapshot.value objectForKey:@"picture"];
+                    NSURL *url = [NSURL URLWithString:imageUrl];
+                    
+                    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+                    [NSURLConnection sendAsynchronousRequest:request
+                                                       queue:[NSOperationQueue mainQueue]
+                                           completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
+                                               if ( !error )
+                                               {
+                                                   UIImage *image = [[UIImage alloc] initWithData:data];
+                                                   imageView.image = image;
+                                               } else{
+                                                   NSLog(@"Error @%", error);                                               }
+                                           }];
+                    
+                }
+            } withCancelBlock:^(NSError *error) {
+                NSLog(@"%@", error.description);
+            }];
+        }
+        
+        
+    } withCancelBlock:^(NSError *error) {
+        NSLog(@"%@", error.description);
+    }];
+    
 }
 
 @end
--- just-query/QuestionViewController.m
+++ just-query/QuestionViewController.m
@@ -168,6 +168,8 @@
         UIButton *pressedButton = (UIButton *)sender;
         ac.questionBodyDescription = [self.questions[pressedButton.tag] questionDescription];
         ac.questionKey = [self.questions[pressedButton.tag] questionKey];
+        ac.questionUserIdKey = [self.questions[pressedButton.tag] userId];
+
     } else if ([segue.destinationViewController isKindOfClass:[UserProfileViewController class]]) {
         UserProfileViewController *ac = (UserProfileViewController *)segue.destinationViewController;
        
